c6720216092db775b020bca6e3c5050c666225f8
dgliwa <dgliwa@luc.edu>
2014-06-16 06:41:50 -0500

    first test passed better

--- src/main/java/com/thoughtworks/accountbalance/AccountBalance.java
+++ src/main/java/com/thoughtworks/accountbalance/AccountBalance.java
@@ -4,16 +4,17 @@ package com.thoughtworks.accountbalance;
  * Created by derekgliwa on 6/16/14.
  */
 public class AccountBalance {
+    private int balance;
     public AccountBalance(int balance){
-
+        this.balance = balance;
     }
 
     public int getBalance(){
-        return 150;
+        return balance;
     }
 
     public void deposit(int depositAmt){
-
+        balance +=depositAmt;
     }
 
     public void withdraw(int withdrawAmt){
